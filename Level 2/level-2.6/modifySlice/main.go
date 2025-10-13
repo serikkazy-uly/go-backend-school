6748cafaaf93a17d2b2fca5574ae679689ae5344
Дамир Карбозов <damir.karbozov@kmf.kz>
2025-10-13 09:03:15 +0500

    перебазировал задачу на место в левел 2 директорию

--- Level 2/level-2.6/modifySlice/main.go
+++ Level 2/level-2.6/modifySlice/main.go
@@ -14,8 +14,12 @@ func main() {
 
 func modifySlice(i []string) {
 	i[0] = "3"
-	i = append(i, "4")
+	// fmt.Println(len(i), cap(i)) // 3 3
+	i = append(i, "4")               // modifies the slice header, not the underlying array
+	fmt.Println("new slice:", i[:6]) // [3 2 3 4]
 
-	i[1] = "5"
+	i[1] = "5" // modifies the underlying array
+	// fmt.Println(len(i), cap(i)) // 4 6 new underlying array allocated
 	i = append(i, "6")
+	// fmt.Println("new slice:", i[:6]) // [3 5 3 4 6]
 }
